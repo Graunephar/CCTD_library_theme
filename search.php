e449e0639f3b9aa5b414e486f47b14206c6a1154
Graunephar <daniel@graungaard.com>
2019-05-23 12:02:09 +0200

    Added serach bar to serach page, and title

--- search.php
+++ search.php
@@ -6,19 +6,21 @@ Template Name: Search Page
 
 <?php get_header(); ?>
 
+<div class="search-page-container">
 
-<?php get_search_form(); ?>
+<?php get_template_part( 'template_parts/search/search', 'bar' ); ?>
 
-
+</div>
 <?php global $wp_query; ?>
 
 
-    This is serach
-
-
+<?php if ( get_search_query( false ) != "" ): ?>
     <h1 class="search-title"> <?php echo $wp_query->found_posts; ?>
-		<?php _e( 'Search Results Found For', 'locale' ); ?>: "<?php the_search_query(); ?>" </h1>
+<?php _e( ' forløb fundet med ', 'locale' ); ?>: "<?php the_search_query(); ?>" </h1>
 
+<?php else: ?>
+    <h1 class="search-title"> <?php echo $wp_query->found_posts; ?> <?php _e( ' forløb fundet', 'locale' ); ?></h1>
+<?php endif; ?>
 
 <?php get_template_part( 'template_parts/list/list', 'posts' ); ?>
 
