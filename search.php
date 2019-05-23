ce252745748b1bdbd58e58fb013f165f077b3e18
Graunephar <daniel@graungaard.com>
2019-05-23 14:49:55 +0200

    Serach made temporarily responsive

--- search.php
+++ search.php
@@ -6,20 +6,25 @@ Template Name: Search Page
 
 <?php get_header(); ?>
 
-<div class="search-page-container">
+    <div class="search-page-aligner d-flex">
 
-<?php get_template_part( 'template_parts/search/search', 'bar' ); ?>
+        <div class="search-page-container">
 
-</div>
+			<?php get_template_part( 'template_parts/search/search', 'bar' ); ?>
+
+        </div>
+
+    </div>
+
 <?php global $wp_query; ?>
 
 
 <?php if ( get_search_query( false ) != "" ): ?>
     <h1 class="search-title"> <?php echo $wp_query->found_posts; ?>
-<?php _e( ' forløb fundet med ', 'locale' ); ?>: "<?php the_search_query(); ?>" </h1>
+		<?php _e( ' forløb fundet med ', 'locale' ); ?>: "<?php the_search_query(); ?>" </h1>
 
 <?php else: ?>
-    <h1 class="search-title"> <?php echo $wp_query->found_posts; ?> <?php _e( ' forløb fundet', 'locale' ); ?></h1>
+    <h1 class="search-title"> <?php echo $wp_query->found_posts; ?><?php _e( ' forløb fundet', 'locale' ); ?></h1>
 <?php endif; ?>
 
 <?php get_template_part( 'template_parts/list/list', 'posts' ); ?>
