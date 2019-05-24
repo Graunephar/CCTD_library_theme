da49a8087d2ac414abde3da068bba6bb0cd070b2
Graunephar <daniel@graungaard.com>
2019-05-24 20:54:16 +0200

    File styling added

--- template_parts/content/post-files.php
+++ template_parts/content/post-files.php
@@ -1,28 +1,32 @@
 
-<div class="download-link-container col-xl-2">
-	<?php
-	$files = rwmb_meta( 'cctd-upload-prefix-file_input' );
-	if ( $files ) :
-		?>
-		<div class="label-wrapper">
-			<span class="upload-label">Filer i forløbet</span>
-		</div>
+
+
+    <div class="download-link-container col-sm-12 col-md-6 col-lg-6 col-xl-3 ">
 		<?php
-
-		//var_dump( $files );
-		foreach ( $files as $file_container ) :
-			//var_dump( $file_container );
-			foreach ( $file_container as $file ) :
-				?>
-
-				<a class="btn btn-primary btn-lg download-link"
-				   href="<?php echo $file['url']; ?>"><?php echo $file['name']; ?></a>
-
+		$files = rwmb_meta( 'cctd-upload-prefix-file_input' );
+		if ( $files ) :
+			?>
+            <div class="label-wrapper">
+                <span class="upload-label">Filer i forløbet</span>
+            </div>
 			<?php
+
+			//var_dump( $files );
+			foreach ( $files as $file_container ) :
+				//var_dump( $file_container );
+				foreach ( $file_container as $file ) :
+					?>
+
+                    <a class="btn btn-primary btn-lg download-link"
+                       href="<?php echo $file['url']; ?>"><?php echo $file['name']; ?></a>
+
+				<?php
+				endforeach;
 			endforeach;
-		endforeach;
-	endif;
-	?>
-</div>
+		endif;
+		?>
+    </div>
+
+
 
 
